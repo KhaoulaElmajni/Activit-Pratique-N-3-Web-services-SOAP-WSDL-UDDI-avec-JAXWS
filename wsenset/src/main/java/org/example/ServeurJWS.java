6b11a3fc880180d49745080207b7bffafcf950bd
Khaoula Elmajni <elmajnikhaoula99@gmail.com>
2022-10-26 21:57:55 +0100

    soap wsdl server and client

--- wsenset/src/main/java/org/example/ServeurJWS.java
+++ wsenset/src/main/java/org/example/ServeurJWS.java
@@ -6,6 +6,6 @@ import org.example.ws.BanqueService;
 public class ServeurJWS {
     public static void main(String[] args) {
         Endpoint.publish("http://0.0.0.0:8085/",new BanqueService());
-        System.out.println("Web Service déployé !");
+        System.out.println("Web Service déployé sur l'adresse http://0.0.0.0:8085/!");
     }
 }
--- wsenset/src/main/java/org/example/ws/BanqueService.java
+++ wsenset/src/main/java/org/example/ws/BanqueService.java
@@ -4,15 +4,29 @@ import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
 
-//POJO Plain Old Java Object
-@WebService(serviceName = "BanqueWs")
+import java.util.Date;
+import java.util.List;
+
+//POJO Plain Old Java Object ==> simple classe java
+@WebService(serviceName = "BanqueWS")
 public class BanqueService {
     @WebMethod(operationName = "Convert")
-    public double conversion(@WebParam double mt){
-        return mt*11;
+    public double conversion(@WebParam(name = "montant") double mt){
+        return mt*11.01;
     }
 
-    public Compte getCompte(int code){
-        return new Compte();
+    @WebMethod
+    public Compte getCompte(@WebParam(name = "code") int code){
+        return new Compte(code,new Date(),Math.random()*9888);
+    }
+
+    @WebMethod
+    public List<Compte> comptes(){
+        return List.of(
+        new Compte(1,new Date(),Math.random()*9888),
+        new Compte(2,new Date(),Math.random()*9888),
+        new Compte(3,new Date(),Math.random()*9888)
+
+        );
     }
 }
